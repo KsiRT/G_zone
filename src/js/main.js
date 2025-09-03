9df3c9e6e75135acf8bce37aacce1678384a1552
ksirt <makarishin1@gmail.com>
2025-09-03 22:50:24 +0300

    Hotfix

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const message = `
-🛍 <b>Нове замовлення – МІДІ--сукня </b>\n
+🛍 <b>Нове замовлення – Сукня-комбез Click rkot831 </b>\n
 👤 Ім’я: <b>${name}</b>\n
 📞 Телефон: <b>${phone}</b>\n
 🏠 Адреса доставки: <b>${address}</b>\n
